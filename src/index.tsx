4f3c84dd10f0168b09ce4765399043d9d124844a
Nicolas Morel <nmr.morel@gmail.com>
2022-08-02 22:11:36 +0200

    Update to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
 import { App } from "./App";
 import "./app.css";
+import { createRoot } from "react-dom/client";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Container not found");
+}
+const root = createRoot(container);
+root.render(<App />);
